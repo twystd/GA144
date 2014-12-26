ed2a44039cc4720791147e4f85db064983ffacbc
Tony Seebregts <tony.seebregts@gmail.com>
2014-12-26 21:04:06 +0200

    erlang: ga144 skeleton

---
+++ erlang/src/ga144.erl
@@ -0,0 +1,17 @@
+-module(ga144).
+
+-export([init/0]).
+
+% INCLUDES
+
+-include    ("include/f18A.hrl").
+-include    ("include/opcode.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+% API
+
+init() ->
+   ok.
+
+% EUNIT
+%
