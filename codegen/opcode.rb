a6d69db4fe580f4ec9e893e615745fb2ab527b98
tonys <tony.seebregts@gmail.com>
2015-01-23 19:34:24 +0200

    codegen: dunno, something

--- codegen/opcode.rb
+++ codegen/opcode.rb
@@ -10,7 +10,32 @@ class OpCode
   end
 
   def parse(spec)
+    tokens = tokenize(spec)
+    puts "TOKENS"
+    puts tokens
     spec 
   end
 
+  def tokenize(string)
+    tokens = Array.new
+    state  = :idle
+    string.each_line do | line |
+      index  = 0
+      while index < string.length do
+        ch     = string[index]
+        index += 1
+
+        case state
+        when :idle
+          case ch
+          when " "
+          when "\n"
+          else
+            puts "-- " + ch
+          end
+        end
+      end
+    end
+    tokens
+  end
 end
